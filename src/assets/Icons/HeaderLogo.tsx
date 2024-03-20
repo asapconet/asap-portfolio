1c6a689c12e14fc08891e71aa2d502dadb632426
ASAP_A1 <aaronrickymeek@gmail.com>
2024-03-20 11:26:13 +0100

    chore: add icons and images

---
+++ src/assets/Icons/HeaderLogo.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const HeaderLogo = () => {
+  return (
+    <svg
+      width="22"
+      height="32"
+      viewBox="0 0 52 52"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M39 0H26V13H13H0V26V39V52H13H26V39H39H52V26V13V0H39ZM13 39H26V26H39V13H26V26H13V39Z"
+        fill="white"
+      />
+    </svg>
+  );
+};
