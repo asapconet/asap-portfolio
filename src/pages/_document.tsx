e7ac34ce9aa7e57d7b2ada60bb7166899c0674fe
ASAP_A1 <aaronrickymeek@gmail.com>
2026-09-18 23:54:53 +0100

    chore: update site preivew data

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,8 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-
+        <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
 
         <link rel="manifest" href="/site.webmanifest" />
 
